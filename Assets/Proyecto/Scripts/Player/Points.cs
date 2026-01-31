e0331f1da9c92826b4dc070e4b83f31e09cef869
juanDavid2301 <castellanosgonzalezj1@gmail.com>
2026-01-31 10:59:05 -0500

    COmit

--- Assets/Proyecto/Scripts/Player/Points.cs
+++ Assets/Proyecto/Scripts/Player/Points.cs
@@ -6,8 +6,15 @@ public class Points : MonoBehaviour
     public PlayerController player;
     public GameObject prefabParticulasDeResiliencia;
     private GameObject instanciaParticulas;
+    public Transform spowner;
 
-
+    private void Update()
+    {
+        if(gameObject.transform.position.x ==spowner.transform.position.x)
+        {
+            gameObject.SetActive(true);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
