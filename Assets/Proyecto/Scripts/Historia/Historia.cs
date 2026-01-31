4a16436ec684045c392c6dfb2e9faaf16c7e40f8
juanDavid2301 <castellanosgonzalezj1@gmail.com>
2026-01-31 13:43:21 -0500

    SceneaCompleta
    Kevin

---
+++ Assets/Proyecto/Scripts/Historia/Historia.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Historia : MonoBehaviour
+{
+    public List<GameObject> historia = new List<GameObject>();
+    void Update()
+    {
+   
+    }
+}
